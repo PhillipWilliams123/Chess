983d837bc619f05ec4e2620ca0bc682820079286
PhillipWilliams123 <341124139@gapps.yrdsb.ca>
2025-03-07 20:02:47 -0500

    Important systems added and should be ready for other ChessPiece code

--- app/src/main/java/com/example/Chess/Chess/ChessBoard.java
+++ app/src/main/java/com/example/Chess/Chess/ChessBoard.java
@@ -1,11 +1,23 @@
 package com.example.Chess.Chess;
 
+import com.example.Chess.Globals;
 import com.example.Chess.Vector2;
+
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class ChessBoard
 {
     public static final int boardSize = 8;
     public static ChessPiece[] chessPieces;
+    /**
+     * The id of the chessPiece into the chessPiece array as its position on the board will probably not match the position in the array
+     */
+    public static int[] chessPieceIds;
+    /**
+     * Ids we can use when a piece is "destroyed" or when we want to add another piece
+     */
+    public static Queue<Integer> freeChessPieceIds;
 
     /**
      * Initializes the chess board
@@ -13,25 +25,19 @@ public class ChessBoard
     public static void Init()
     {
         chessPieces = new ChessPiece[boardSize * boardSize];
+        chessPieceIds = new int[boardSize * boardSize];
+        freeChessPieceIds = new LinkedList<>();
 
         //initialize each piece to an empty piece
         for (int i = 0; i < boardSize * boardSize; i++)
         {
            chessPieces[i] = new EmptyPiece();
+           freeChessPieceIds.add(i);
         }
-
-        chessPieces[0] = new Rook();
-        System.out.println(chessPieces[0].GetImageLocation());
-        chessPieces[0].LoadImageToPiece();
-    }
-
-    public static void TestDraw()
-    {
-        chessPieces[0].DrawPiece();
     }
 
     /**
-     * Will take in a position and return an index into the chess board array
+     * Will take in a position and return an index into a array based on positions (chessPieceIds)
      * @param pos the position from 0-8 and 0-8 on x and y
      * @return the index into the chessPieces array
      */
@@ -62,15 +68,84 @@ public class ChessBoard
     public static ChessPiece GetChessPieceAtPos(Vector2 pos)
     {
         int index = PosToIndex(pos);
-        //our index has returned -1 so we return null
-        if(index < 0)
+        if(index == -1)
             return new EmptyPiece();
 
         return chessPieces[index];
     }
 
-    public static Vector2 ScalePosToBoardSpace()
+    /**
+     * Sets the id of a chess piece at the position
+     * @param pos the position to set it at
+     * @param id the id to set it at
+     */
+    public static void SetPieceIdAtPos(Vector2 pos, int id)
     {
+        int index = PosToIndex(pos);
+        if(index == -1)
+            return;
+        chessPieceIds[index] = id;
+    }
 
+    /**
+     * Gets the id of a chess piece at the position, use to access a piece in the chessPiece array
+     * @param pos the position to set it at
+     * @return -1 if the spot is empty or out of bounds, or the pieces id
+     */
+    public static int GetPieceIdAtPos(Vector2 pos)
+    {
+        int index = PosToIndex(pos);
+        if(index == -1)
+            return -1;
+        return chessPieceIds[index];
+    }
+
+    /**
+     * Scales a position into an 8, 8 space, from window space
+     * @return The scaled vector
+     */
+    public static Vector2 ScalePosToBoardSpace(Vector2 pos)
+    {
+        return Vector2.Mul(pos, Vector2.Div(new Vector2(Globals.ScreenWidth, Globals.ScreenHeight), boardSize));
+    }
+
+    /**
+     * Adds a piece onto the board, will do nothing if it's not a valid spot to add a piece
+     * @param piece the chessPiece to add with accept anything that implements the chessPiece class
+     */
+    public static void AddPiece(ChessPiece piece)
+    {
+        //check if it's a valid spot on the board
+        if(!PosInBounds(piece.position))
+            return;
+
+        //check if there's already a piece there
+        ChessPiece checkPiece = GetChessPieceAtPos(piece.position);
+        if(checkPiece.id != -1)
+            return;
+
+        //we are good to add a piece
+        int index = freeChessPieceIds.poll();
+        System.out.println(index);
+        chessPieces[index] = piece.Copy();
+        chessPieces[index].id = index;
+        chessPieces[index].Init();
+        SetPieceIdAtPos(piece.position, index);
+    }
+
+    /**
+     * Will set a piece on the position to empty effectively "deleting it"
+     * @param position
+     */
+    public static void DeletePiece(Vector2 position)
+    {
+        //check if it's a valid spot on the board
+        if(!PosInBounds(position))
+            return;
+
+        int index = PosToIndex(position);
+        int id = GetChessPieceAtPos(position).id;
+        freeChessPieceIds.add(id);
+        chessPieces[index] = new EmptyPiece();
     }
 }
--- app/src/main/java/com/example/Chess/Chess/ChessPiece.java
+++ app/src/main/java/com/example/Chess/Chess/ChessPiece.java
@@ -1,7 +1,6 @@
 package com.example.Chess.Chess;
 import static com.raylib.Raylib.*;
 import com.example.Chess.Vector2;
-import com.raylib.Raylib;
 import com.raylib.Raylib.Texture;
 import static com.raylib.Colors.*;
 
@@ -14,14 +13,26 @@ public abstract class ChessPiece
     public Vector2 position;
     private Texture image;
 
+    public void Init()
+    {
+        LoadImageToPiece();
+    }
+
+    /**
+     * Loads the pieces image
+     */
     public void LoadImageToPiece()
     {
         image = LoadTexture(GetImageLocation());
     }
 
+    /**
+     * Draws the piece at its position
+     */
     public void DrawPiece()
     {
-        DrawTexture(image, 0, 0, WHITE);
+        Vector2 scaledPos = ChessBoard.ScalePosToBoardSpace(position);
+        DrawTexture(image, (int)scaledPos.x, (int)scaledPos.y, WHITE);
     }
 
     /**
@@ -31,10 +42,53 @@ public abstract class ChessPiece
     public abstract String GetImageLocation();
 
     /**
-     * Checks if the piece can make a valid move to a position
+     * Tries to move a piece to a position with its rules implemented in its class
      * @param pos the position to move to
      * @return true if it can make the move, false if it cannot make the move
      */
-    public abstract boolean ValidMove(Vector2 pos);
+    public abstract boolean TryMove(Vector2 pos);
 
+    /**
+     * Checks in a direction from a position to a wanted position, if there is a piece of a border of the chess board
+     * @param dir the direction to go in (Should be integers and whole numbers)
+     * @param pos the position to start from
+     * @param wantedPos the position we want to go to
+     * @return the number of checks it has done (can be multiplied by direction to get the final position it landed on), Will be 0 if we cannot move at all
+     */
+    public int CheckInDirection(Vector2 dir, Vector2 pos, Vector2 wantedPos)
+    {
+
+        //(Might have bugs im not sure, but it works with the rook in my testing)
+
+        int moveCount = 0;
+        Vector2 posMove = pos;
+        while (true)
+        {
+            posMove = Vector2.Add(posMove, dir);
+
+            //if we reach out of bounds we have either gone out of the board or never reached our wanted position
+            if(!ChessBoard.PosInBounds(posMove))
+            {
+                return 0;
+            }
+
+            ChessPiece piece = ChessBoard.GetChessPieceAtPos(posMove);
+            //we have a piece in our moved position so this is the farthest we can go
+            if(piece.id != -1)
+            {
+                return moveCount;
+            }
+
+            //we have reached where we wanted to go with no issues
+            if(posMove.Equals(wantedPos))
+            {
+                moveCount++;
+                return moveCount;
+            }
+
+            moveCount++;
+        }
+    }
+
+    public abstract ChessPiece Copy();
 }
