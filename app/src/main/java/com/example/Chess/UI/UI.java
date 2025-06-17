8fb88da8b487d50c7b39acca46ebd5a60d3b9b62
ok <135392993+DaEloMorld3@users.noreply.github.com>
2025-06-16 23:10:36 -0400

    Update UI.java

--- app/src/main/java/com/example/Chess/UI/UI.java
+++ app/src/main/java/com/example/Chess/UI/UI.java
@@ -1,32 +1,25 @@
 package com.example.Chess.UI;
 
-
-
-
 import com.example.Chess.Chess.ChessBoard;
 import com.example.Chess.Chess.GameState;
-import com.example.Chess.Interaction;
 import com.example.Chess.Network.NetworkManager;
 import com.example.Chess.Network.Packets.StartGamePacket;
 import com.example.Chess.Rendering.Renderer;
 import com.example.Chess.Vector2;
 import com.raylib.Raylib;
 
-
-
 public class UI {
 
     public static UiButton[] buttons;
-public static boolean Gamestart = false;
-public static boolean IsSoundenabled = true;
+    public static boolean Gamestart = false;
+    public static boolean IsSoundenabled = true;
 
     public static boolean IsMenuOpen;
     public static boolean IsMutiMenuOpen;
 
     static ToolTip startGameToolTip = new ToolTip(new Vector2(640, 0), new Vector2(180, 100), "test");
 
-    public static void Initialize()
-    {
+    public static void Initialize() {
         buttons = new UiButton[6];
         buttons[0] = new UiButton(new Vector2(640, 0), new Vector2(180, 100), "Start Game");
 
@@ -35,39 +28,28 @@ public static boolean IsSoundenabled = true;
 
         buttons[3] = new UiButton(new Vector2(820, 0), new Vector2(180, 100), "Quit Game");
 
-
-
     }
 //    SetSoundVolume(Sound sound, float volume);
 
-    public static void updateButtons()
-    {
+    public static void updateButtons() {
         //lock the start game if we are connected to a server
         buttons[0].lock = NetworkManager.isClient && !NetworkManager.isServer;
-        if(NetworkManager.isServer)
-        {
+        if (NetworkManager.isServer) {
             buttons[0].lock = NetworkManager.server.currentClients != 2;
         }
 
-        if(IsMenuOpen)
-        {
+        if (IsMenuOpen) {
             UiMenu.updateButtons();
-        }
-        else if(IsMutiMenuOpen)
-        {
+        } else if (IsMutiMenuOpen) {
             MutiUi.updateButtons();
-        }
-        else
-        {
-            if(buttons[0].IsButtonClicked())
-            {
+        } else {
+            if (buttons[0].IsButtonClicked()) {
                 GameState.Init();
                 ChessBoard.Init();
                 ChessBoard.InitStandardGame();
                 Renderer.Draw2d = true;
 
-                if(NetworkManager.isServer)
-                {
+                if (NetworkManager.isServer) {
                     //tell the other client we are the one starting
                     NetworkManager.server.SendPacketAllExclude(new StartGamePacket(true), 0);
                     GameState.isOurTurn = true;
@@ -75,56 +57,43 @@ public static boolean IsSoundenabled = true;
 
             }
 
-            if(buttons[1].IsButtonClicked())
-            {
+            if (buttons[1].IsButtonClicked()) {
                 IsMenuOpen = true;
 
             }
-            if(buttons[2].IsButtonClicked())
-            {
+            if (buttons[2].IsButtonClicked()) {
                 MutiUi.UpdateServerInfos();
                 IsMutiMenuOpen = true;
             }
-            if(buttons[3].IsButtonClicked())
-            {
+            if (buttons[3].IsButtonClicked()) {
                 Raylib.CloseWindow();
             }
 
-            if(buttons[0].lock)
-            {
-                if(NetworkManager.isClient)
-                {
+            if (buttons[0].lock) {
+                if (NetworkManager.isClient) {
                     startGameToolTip.SetText("Not Server Owner");;
                 }
-                if(NetworkManager.isServer)
-                {
+                if (NetworkManager.isServer) {
                     startGameToolTip.SetText("Not Enough Players");
                 }
 
                 startGameToolTip.show = true;
-            }
-            else
+            } else {
                 startGameToolTip.show = false;
+            }
         }
 
         startGameToolTip.Update();
 
     }
 
-    public static void RenderButtons()
-    {
-        if(IsMenuOpen)
-        {
+    public static void RenderButtons() {
+        if (IsMenuOpen) {
             UiMenu.RenderButtons();
-        }
-        else if(IsMutiMenuOpen)
-        {
+        } else if (IsMutiMenuOpen) {
             MutiUi.RenderButtons();
-        }
-        else
-        {
-            for(UiButton button : buttons)
-            {
+        } else {
+            for (UiButton button : buttons) {
                 if (button != null) {
                     button.DrawButton();
                 }
@@ -134,8 +103,4 @@ public static boolean IsSoundenabled = true;
         startGameToolTip.Draw();
     }
 
-
 }
-
-
-
