6ff332c85c24a227b2d6b5ecdf0c12b5643c331f
ok <135392993+DaEloMorld3@users.noreply.github.com>
2025-06-16 23:06:55 -0400

    Update Queen.java

--- app/src/main/java/com/example/Chess/Chess/Queen.java
+++ app/src/main/java/com/example/Chess/Chess/Queen.java
@@ -79,9 +79,4 @@ public class Queen extends ChessPiece {
     public ChessPiece Copy() {
         return new Queen(this.position, this.side);
     }
-    @Override
-    public int GetPieceType()
-    {
-        return 5;
-    }
 }
